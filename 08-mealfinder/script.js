7eb4e3be07e4dcd3db71975a6d9b0f73803f4782
ivanburda1986 <burda.ivan@gmail.com>
2020-09-06 13:58:29 +0200

    finished the meal project

--- 08-mealfinder/script.js
+++ 08-mealfinder/script.js
@@ -32,7 +32,7 @@ function searchMeal(e) {
             `
           <div class="meal">
             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-            <div class="meal-info data-mealID="${meal.idMeal}"> 
+            <div class="meal-info" data-mealID="${meal.idMeal}"> 
               <h3>${meal.strMeal}</h3>
             </div>
           </div>
@@ -48,6 +48,74 @@ function searchMeal(e) {
 }
 
 
+//Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(response => response.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    })
+};
+
+//Fetch a random meal
+function getRandomMeal() {
+  //Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(response => response.json())
+    .then(data => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    })
+}
+
+//Add a meal to the DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+  <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>`: ""}
+      ${meal.strArea ? `<p>${meal.strArea}</p>`: ""}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+      ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+      </ul>
+    </div>
+  </div>
+  `;
+}
+
 
 //EVENT LISTENERS
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info')
+    } else {
+      return false;
+    }
+  })
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
+});
