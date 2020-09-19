e7a875b74de185e8c5f288e0fa48223d6d260795
Ivan Burda <burda.ivan@gmail.com>
2020-09-20 00:19:12 +0200

    fisniehd the ny countdown project

--- 18-new-year-countdown/script.js
+++ 18-new-year-countdown/script.js
@@ -4,6 +4,7 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
 const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
@@ -16,7 +17,7 @@ function updateCountDown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = (Math.floor(diff / 1000 / 60 / 60) % 24) - 1;
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
@@ -28,7 +29,10 @@ function updateCountDown() {
 }
 
 //Show spinner before the countdown
-setTimeout(() => {}, 1000);
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
 
 //Run the countdown every second
 setInterval(updateCountDown, 1000);
