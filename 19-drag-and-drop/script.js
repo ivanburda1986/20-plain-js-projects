5b9d48787796a1a530f805881ef100f28ee980c4
ivanburda1986 <burda.ivan@gmail.com>
2020-09-20 21:32:21 +0200

    implemented html and css for the drag and drop project

---
+++ 19-drag-and-drop/script.js
@@ -0,0 +1,45 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page'
+]
+
+//Store the list items
+const listItems = [];
+let dragStartIndex;
+createList();
+
+//Insert list items into DOM
+function createList() {
+  [...richestPeople]
+  .map(person => ({
+      value: person,
+      randomNumberForSorting: Math.random()
+    }))
+    .sort((a, b) => a.randomNumberForSorting - b.randomNumberForSorting)
+    .map(sortedPerson => sortedPerson.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
+      listItem.setAttribute('data-index', index);
+      listItem.innerHTML = `
+    <span class="number">${index+1}</span>
+    <div class="draggable" draggable="true">
+      <p class="person-name">${person}</p>
+      <i class="fas fa-grip-lines"></i>
+    </div>
+    `
+
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
+}
